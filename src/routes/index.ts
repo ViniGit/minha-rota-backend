6e59149c05a2273ce9ebec55fa105bde08bfa627
Vinicius Correia <vinicius@buzzlead.com.br>
2022-08-17 22:30:10 -0300

    fix routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Router } from "express"
 import { authenticateRoutes } from "./authenticate.routes"
-import { categoriesRoutes } from "./categories.routes"
-import { specificationsRoutes } from "./specifications.routes"
 import { usersRoutes } from "./users.routes"
 import { meRoutes } from "./me.routes"
 
 const router = Router()
 
-router.use("/categories", categoriesRoutes)
-router.use("/specifications", specificationsRoutes)
 router.use("/users", usersRoutes)
 router.use(authenticateRoutes)
 router.use(meRoutes)
